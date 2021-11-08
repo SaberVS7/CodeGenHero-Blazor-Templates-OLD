4cb91e59adea0027f7bc8c32fde2322ffda320e7
Robert Schroeder <RobertS@msctek.com>
2021-11-08 16:02:34 -0600

    #853 - Implementation of DTO Generator

--- src/net5.0/Generators/DTOGenerator.cs
+++ src/net5.0/Generators/DTOGenerator.cs
@@ -24,12 +24,85 @@ namespace CodeGenHero.Template.Blazor5.Generators
             bool dtoIncludeRelatedObjects,
             string className)
         {
+            var primaryKeys = GetPrimaryKeys(entity);
+            var entityProperties = entity.GetProperties();
+
             StringBuilder sb = new StringBuilder();
             sb.Append(GenerateHeader(usings, classNamespace));
+            sb.Append(GenerateConstructor(className));
 
+            foreach(var property in entityProperties)
+            {
+                string cType = GetCType(property);
+                var simpleType = ConvertToSimpleType(cType);
+                string propertyName = property.Name;
+                bool isPrimaryKey = primaryKeys.Any(x => x.Equals(propertyName));
 
+                if (isPrimaryKey)
+                {
+                    sb.AppendLine($"\t\t// Primary Key");
+                }
+                sb.AppendLine($"\t\tpublic {simpleType} {Inflector.Pascalize(propertyName)} {{ get; set; }}");
+                sb.AppendLine(string.Empty);
+            }
+
+            sb.Append(GenerateReverseNavigation(dtoIncludeRelatedObjects, entity, excludedEntityNavigations));
+
+            sb.AppendLine("\t\tpartial void InitializePartial();");
 
             sb.Append(GenerateFooter());
+            return sb.ToString();
+        }
+
+        private string GenerateConstructor(string className)
+        {
+            IndentingStringBuilder sb = new IndentingStringBuilder(2);
+
+            sb.AppendLine($"public {className}()");
+            sb.AppendLine("{");
+            sb.AppendLine("\tInitializePartial();");
+            sb.AppendLine("}");
+            sb.AppendLine(string.Empty);
+
+            return sb.ToString();
+        }
+
+        private string GenerateReverseNavigation(bool includeRelatedObjects,
+            IEntityType entity,
+            IList<IEntityNavigation> excludedNavigationProperties)
+        {
+            if (!includeRelatedObjects)
+            {
+                return string.Empty;
+            }
+
+            SortedSet<IForeignKey> foreignKeyList = entity.ForeignKeys;
+            var excludedNavigations = excludedNavigationProperties.Select(x => x.Navigation).ToList();
+            var excludedForeignKeys = GetForeignKeyNames(excludedNavigations);
+
+            IndentingStringBuilder sb = new IndentingStringBuilder(2);
+
+            foreach (var reverseFK in entity.Navigations)
+            {
+                var keyName = GetForeignKeyName(reverseFK);
+                bool preventCircularReference = string.IsNullOrWhiteSpace(keyName) ? false : excludedForeignKeys.Contains(keyName);
+
+                if (preventCircularReference)
+                {
+                    sb.Append("// ");
+                }
+
+                if (!reverseFK.ClrType.Name.Equals("ICollection`1"))
+                {
+                    sb.Append($"public virtual {reverseFK.ForeignKey.PrincipalEntityType.ClrType.Name} {Inflector.Pascalize(reverseFK.Name)} {{ get; set; }} // One to One mapping"); // Foreign Key
+                }
+                else
+                {
+                    sb.Append($"public virtual System.Collections.Generic.ICollection<{reverseFK.ForeignKey.DeclaringEntityType.ClrType.Name}> {reverseFK.Name} {{ get; set; }} = new List<{reverseFK.ForeignKey.DeclaringEntityType.ClrType.Name}>(); // Many to many mapping"); // Foreign Key
+                }
+                sb.AppendLine(string.Empty);
+            }
+
             return sb.ToString();
         }
     }
